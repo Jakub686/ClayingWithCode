db84457410651a556a7632f1d8c9a332e7cec960
Jakub <jakub.bchyla@gmail.com>
2022-05-30 10:26:00 +0200

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
         HashMap<String, Object> map = new HashMap<>();
         //json
         Person person1 = new Person("a","b","c",new Address("aaa","bbb"));
@@ -12,8 +19,21 @@ public class Main {
         Person person2 = person1;
 
         map.put("1",person2);
+        map.put("2","hello");
         Object obj = map.get("1");
         System.out.println(obj);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String jsonStr = mapper.writeValueAsString(map);
+        System.out.println("Json before save in a file" + jsonStr);
+
+        //write json into file
+        BufferedWriter write = new BufferedWriter(new FileWriter("C:\\ClayingWithCode\\src\\main\\java\\db.json"));
+        write.write(jsonStr);
+        write.close();
+        }catch (IOException e){
+        }
 
     }
 }
